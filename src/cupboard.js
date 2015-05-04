7c981017f26863f0c08a9ba34c2ed66612cff9d7
Jesse Millar <hellojessemillar@gmail.com>
2015-05-04 15:58:27 -0600

    Reformatting, linting, and organizing into multiple files/classes

---
+++ src/cupboard.js
@@ -0,0 +1,48 @@
+var Cupboard = function() {
+    if (typeof(Storage) == 'undefined') {
+        console.warn('This browser does not support localStorage');
+    }
+
+    this.save = function(key, value) {
+        var tempValue;
+
+        if (typeof value == 'object') {
+            tempValue = JSON.stringify(value);
+        } else {
+            tempValue = value;
+        }
+
+        sessionStorage.setItem(key, tempValue);
+
+        return true;
+    };
+
+    this.get = function(key) {
+        if (sessionStorage.getItem(key)) {
+            try {
+                return JSON.parse(sessionStorage.getItem(key));
+            } catch (err) {
+                return sessionStorage.getItem(key);
+            }
+        } else {
+            return undefined;
+        }
+
+    };
+
+    this.delete = function(key) {
+        if (sessionStorage.getItem(key)) {
+            sessionStorage.removeItem(key);
+
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.clear = function() {
+        sessionStorage.clear();
+
+        return true;
+    };
+};
---
+++ src/vault.js
@@ -0,0 +1,47 @@
+var Vault = function() {
+    if (typeof(Storage) == 'undefined') {
+        console.warn('This browser does not support localStorage');
+    }
+
+    this.save = function(key, value) {
+        var tempValue;
+
+        if (typeof value == 'object') {
+            tempValue = JSON.stringify(value);
+        } else {
+            tempValue = value;
+        }
+
+        localStorage.setItem(key, tempValue);
+
+        return true;
+    };
+
+    this.get = function(key) {
+        if (localStorage.getItem(key)) {
+            try {
+                return JSON.parse(localStorage.getItem(key));
+            } catch (err) {
+                return localStorage.getItem(key);
+            }
+        } else {
+            return undefined;
+        }
+    };
+
+    this.delete = function(key) {
+        if (localStorage.getItem(key)) {
+            localStorage.removeItem(key);
+
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    this.clear = function() {
+        localStorage.clear();
+
+        return true;
+    };
+};
